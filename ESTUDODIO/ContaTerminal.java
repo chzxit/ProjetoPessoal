e1d9c39e40e41a69834fecf642da80d154501358
Caio <njtrio7@gmail.com>
2024-06-15 18:50:37 -0200

    git push

---
+++ ESTUDODIO/ContaTerminal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ContaTerminal{
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        //TODO:Conhecer e importar a classe Scanner 
+
+        //TODO:Exibir as mensagens paras os nossos usuarios
+
+      
+        //TODO:obter pelo scanner os valores digitados pelo terminal 
+
+        //TODO:Exirbir a menssgem final
+
+    }
+}
